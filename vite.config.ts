2e6f9f5726343c877b903387d41f6b32865f24d5
3lackrukh <155496966+3lackrukh@users.noreply.github.com>
2025-02-12 11:19:29 -0600

    Explicit outDir and assetsDir, adds sourcemapping for debugging

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '',
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf'],
   optimizeDeps: {
     include: ['three', '@react-three/fiber', '@react-three/drei']
   },
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
