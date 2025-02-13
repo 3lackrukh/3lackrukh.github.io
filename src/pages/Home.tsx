fb8c9abf29d06c5883a522f4de1cf48f5fbad163
3lackrukh <nathan.rhys@atlasschool.com>
2025-02-13 03:58:18 -0600

    Element ordering fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,12 +20,12 @@ const Home = () => {
       {/* Banner Section */}
       <div className="relative h-screen">
         {/* Full-screen StellaOctangula background */}
-        <div className="absolute -inset-1/3 scale-150">
+        <div className="absolute -inset-1/3 scale-150 z-0">
           <StellaOctangula />
         </div>
         
         {/* Overlay gradient */}
-        <div className="absolute inset-1/3 bg-gradient-to-b from-gray-900/30 via-gray-900/70 to-gray-900" />
+        <div className="absolute inset-1/3 bg-gradient-to-b from-gray-900/30 via-gray-900/70 to-gray-900 z-1" />
 
         {/* Headshot that stays fixed while scrolling */}
         <motion.div 
